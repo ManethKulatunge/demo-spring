9ad1c12ce5073b0dc4fd3362f54a4420cc1afd56
Maneth <maneth.kulatunge@mail.mcgill.ca>
2021-08-29 20:29:05 -0400

    Student Class Made

--- src/main/java/com/example/demospring/DemoSpringApplication.java
+++ src/main/java/com/example/demospring/DemoSpringApplication.java
@@ -1,9 +1,14 @@
 package com.example.demospring;
 
+import com.example.demospring.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,8 +19,16 @@ public class DemoSpringApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<Student> hello() {
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.jamal@gmail.com",
+						21,
+						LocalDate.of(2000, Month.JANUARY, 5)
+				)
+		);
 	}
 
 }
